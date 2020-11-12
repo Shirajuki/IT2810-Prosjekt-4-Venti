153a61d2c4c6c319077495588cb22b27815292eb
heleneelling <helene.elling@outlook.com>
2020-11-12 12:06:53 +0100

    fixed problems #11

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -47,30 +47,32 @@ const App = observer(( props: IProps ) => {
 	}, [props.modal.id]);
 
   return (
-    <SafeAreaView style={styles.container}>
-		<View style={styles.modalHeader}>
-			<TouchableOpacity style={styles.closeButton} data-cy="close-button" onPress={() => Alert.alert("closing")}>
-				<Text>&#10006;</Text>
-			</TouchableOpacity>
-		</View>
-		<Items id={props.modal.product?.id} img={product?.image_link} name={product?.name} description={product?.description} rating={Number(product?.rating)} price={product?.price} onClick={() => void(0)} type="modal"/>
-		<View /*style={"star"}*/>
-			<StarRating data-cy="star-area" roundedCorner={true} isHalfRating={true}  handleOnClick={(rating:number) => {setStars(rating)}}/>
-		</View>
-		<View style={styles.reviews}>
-			<View style={styles.reviewInput}>
-				<textarea ref={nameRef} placeholder="Name" data-cy="name-area"></textarea>
-				<StarRating size={20} handleOnClick={handleOnClick} isHalfRating={true}/>
-				<textarea ref={messageRef} placeholder="Write your review here..." data-cy="review-area"></textarea>
-				<TouchableOpacity style={styles.sendButton} onPress={() => post()} data-cy="send-area"><Text>Send</Text></TouchableOpacity>
+    <SafeAreaView style={styles.modalContainer}>
+		<View style={styles.modalContainer}>
+			<View>
+				<TouchableOpacity style={styles.closeButton} data-cy="close-button" onPress={() => Alert.alert("closing")}>
+					<Text>&#10006;</Text>
+				</TouchableOpacity>
 			</View>
-			{CTX.reviewStore.reviews.map((review: Review) => (
-				<View style={styles.review} data-cy="view-reviews">
-					<Text style={styles.reviewUser}>{review.name}</Text>
-					<StarRating size={20} initialRating={review.stars} isReadOnly={true} isHalfRating={true}/>
-					<Text style={styles.reviewComment}>{review.reviewText}</Text>
+			<Items id={props.modal.product?.id} img={product?.image_link} name={product?.name} description={product?.description} rating={Number(product?.rating)} price={product?.price} onClick={() => void(0)} type="modal"/>
+			<View /*style={"star"}*/>
+				<StarRating data-cy="star-area" roundedCorner={true} isHalfRating={true}  handleOnClick={(rating:number) => {setStars(rating)}}/>
+			</View>
+			<View style={styles.reviews}>
+				<View style={styles.reviewInput}>
+					<textarea ref={nameRef} placeholder="Name" data-cy="name-area"></textarea>
+					<StarRating size={20} handleOnClick={handleOnClick} isHalfRating={true}/>
+					<textarea ref={messageRef} placeholder="Write your review here..." data-cy="review-area"></textarea>
+					<TouchableOpacity style={styles.sendButton} onPress={() => post()} data-cy="send-area"><Text>Send</Text></TouchableOpacity>
 				</View>
-				))}
+				{CTX.reviewStore.reviews.map((review: Review) => (
+					<View style={styles.review} data-cy="view-reviews">
+						<Text style={styles.reviewUser}>{review.name}</Text>
+						<StarRating size={20} initialRating={review.stars} isReadOnly={true} isHalfRating={true}/>
+						<Text style={styles.reviewComment}>{review.reviewText}</Text>
+					</View>
+					))}
+			</View>
 		</View>
       	<Text>Hello world </Text>
       	<StatusBar style="auto" />
@@ -105,7 +107,7 @@ const styles = StyleSheet.create({
 		height: height_Content
 	},
 	modalHeader: {
-		fontSize: 10,
+		//fontSize: 10,
 	},
 	closeButton: {
 		padding: 10,
