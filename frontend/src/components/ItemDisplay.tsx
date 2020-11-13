e78bcf27f0ad3df12111d1802dedacb68a6143a6
heleneelling <helene.elling@outlook.com>
2020-11-13 14:38:39 +0100

    itemDisplay fikset

--- frontend/src/components/ItemDisplay.tsx
+++ frontend/src/components/ItemDisplay.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Product from "../models/product";
-import { View } from 'react-native';
+import { FlatList} from 'react-native';
 import Items from './Items';
 
 interface IProps {
@@ -10,13 +10,11 @@ interface IProps {
 
 const ItemDisplay = (props: IProps) => {
 	return (
-		<>
-		<View>
-			{props.itemList.map(item => (
-				<Items key={item.id} id={item.id} img={item.image_link} name={item.name} description={item.description} rating={item.rating} price={item.price} type="" onClick={() => props.setModal(item.id, item) } />
-			))}
-		</View>
-	</>
+		<FlatList data={props.itemList}
+			renderItem={({item}) => {
+				return <Items key={item.id} id={item.id} img={item.image_link} name={item.name} description={item.description} rating={item.rating} price={item.price} type="" onClick={() => props.setModal(item.id, item) } />
+			}}
+		/>
 	);
 }
 
